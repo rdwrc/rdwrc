baa3f3f298e807aae2cf3f83aafab4d89a9f30b0
Rick Evans <javaswinger@yahoo.com>
2013-09-07 13:41:01 -0400

    initial rails project working with mySQL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rdwrc::Application.config.secret_token = '9d4d4d9607815cb2763848587d853d4020d30915c626083d96f5275c6d5a5508370f3f8a43fe4b578d307dec9248331886509852daf005c79cfd908ba9be1e98'
