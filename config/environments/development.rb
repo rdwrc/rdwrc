0b672735f185493149dd406167a75cc5d9d43aa4
Rick Evans <javaswinger@yahoo.com>
2013-09-07 13:50:38 -0400

    add devise for user/admin logins

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Rdwrc::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # DEVISE settings
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,7 @@ Rdwrc::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # DEVISE settings
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
