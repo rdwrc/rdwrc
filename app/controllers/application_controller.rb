daf18a0e104223ad673bcbe88e0b5d1bc36956f1
Rick Evans <javaswinger@yahoo.com>
2013-11-17 08:54:29 -0500

    inspect user role on login and redirect them to correct place

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) ||
+      if resource.is_a?(User)
+        if resource.admin?
+          admin_url
+        elsif resource.editor?
+          editor_url
+        elsif resource.author?
+          author_url
+        elsif resource.contributor?
+          contributor_url
+        else
+          super
+        end
+      else
+        super
+      end
+  end
 end
